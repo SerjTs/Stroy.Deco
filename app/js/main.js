a7cefd92a89365d3455fd9976a279d25b5b54cb6
SerjTs <postwork@mail.ru>
2021-02-07 13:17:00 +0300

    added top section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function() {
 
+  $('.top__slider').slick({
+    autoplay: false,
+    autoplaySpeed: 10000,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../images/arrow-prev.png" alt="кнопка стрелка влево"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../images/arrow-next.png" alt="кнопка стрелка вправо"></button>',
+  });
+
 });
