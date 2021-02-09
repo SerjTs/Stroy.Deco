fd946197a71c6243658e7d0848640a814f80f09c
SerjTs <postwork@mail.ru>
2021-02-09 16:26:08 +0300

    added about section

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,14 @@ $(function() {
     nextArrow:
       '<button type="button" class="slick-next"><img src="../images/arrow-next.png" alt="кнопка стрелка вправо"></button>',
   });
+  
+  $('.about__slider').slick({
+    autoplay: true,
+    autoplaySpeed: 10000,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../images/arrow-prev-small.png" alt="кнопка стрелка влево"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../images/arrow-next-small.png" alt="кнопка стрелка вправо"></button>',
+  });
 
 });
