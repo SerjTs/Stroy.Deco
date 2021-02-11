d06eda1a6d1b39704b1b532896c9f0c4db9ac566
SerjTs <postwork@mail.ru>
2021-02-11 14:06:50 +0300

    added faq section

--- app/js/main.js
+++ app/js/main.js
@@ -18,4 +18,13 @@ $(function() {
       '<button type="button" class="slick-next"><img src="../images/arrow-next-small.png" alt="кнопка стрелка вправо"></button>',
   });
 
+  $('.accordion__header').on('click', function() {
+    $('.accordion__text').slideUp();
+    $(this).next().slideToggle();
+    $('.accordion__header').removeClass('accordion__header--active');
+    $(this).toggleClass('accordion__header--active');
+  });
+
+  
+
 });
