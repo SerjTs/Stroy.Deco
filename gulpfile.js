738ac3e1a034e70c207b61a9a518605dce900a1e
SerjTs <postwork@mail.ru>
2021-02-20 19:50:04 +0300

    site is typed and validated

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,8 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-  .pipe(scss({outputStyle: 'compressed'}))
   // .pipe(scss({outputStyle: 'expanded'}))
+  .pipe(scss({outputStyle: 'compressed'}))
   .pipe(concat('style.min.css'))
   .pipe(autoprefixer({
     overrideBrowserslist: ['last 10 versions'],
